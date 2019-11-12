c7828a286fe3c0a106f447ad115f2a66f1df3032
Valentintoussatoussa <56109276+Valentintoussatoussa@users.noreply.github.com>
2019-11-12 23:22:30 +0100

    Add files via upload

---
+++ Eval/Eval/Eval2019.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using ChatonsCodeFirst.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatonsCodeFirst.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db=new ContexteBDD())
+            {
+                var categorie = db.Categories.First(c => c.Nom == "Mignons");
+
+                var chaton = new Chaton()
+                {
+                    Nom="Mimi",
+                    Couleur ="Roux",
+                    Stérilisé =true,
+                    Categorie =categorie
+                };
+                
+
+                var proprio = new Proprietaire() {
+                    Nom = "Ferronnière",
+                    Prenom="Alan"
+                };
+                proprio.Chatons.Add(chaton);
+                //ou
+                chaton.Proprietaires.Add(proprio);
+
+                db.Chatons.Add(chaton);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Eval/Eval/Eval2019.ORM/Intervention.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eval2019.ORM
+{
+    public class Intervention
+    {
+        public Intervention()
+        {
+            
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Ouverture { get; set; }
+
+        [Required]
+        public DateTime Fermeture { get; set; }
+
+        public virtual Intervenant Intervenant { get; set; }
+        public virtual Materiel Materiel { get; set; }
+        public virtual Vehicule Vehicule { get; set; }
+
+    }
+}
